4af3f0f180d88e7d03cb560dda609d9650f4f6a3
JsPako <jacobswiniarski@gmail.com>
2020-05-22 15:19:30 +0100

    Update Commit

--- main.py
+++ main.py
@@ -5,19 +5,53 @@ from PIL import Image, ImageGrab
 from pynput.mouse import Button, Controller
 
 
+def mouseMove():
+    original = mouse.position
+    for x in range(80000):
+        x = x + 1
+        mouse.position = (135, 942)
+        if x == 79999:
+            mouse.position = (135, 942)
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            break
+    for x in range(8000):
+        x = x + 1
+        mouse.position = (135, 942)
+        if x == 7999:
+            mouse.position = original
+            break
+
+
+def colourMatch(masked_image):
+    thresh = cv2.threshold(masked_image, 0, 255,
+                           cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    pixels = cv2.countNonZero(thresh)
+    if pixels >= 500:
+        mouseMove()
+    else:
+        return True
+
+
 def processImage(original_image):
     mask_Image = cv2.inRange(original_image, lower_range, upper_range)
-    cv2.imshow('Mask', mask_Image)
-    cv2.imshow('image', original_image)
+    colourMatch(mask_Image)
 
+
+mouse = Controller()
 upper_range = np.array([60, 255, 255])
-lower_range = np.array([20, 70, 70])
+lower_range = np.array([20, 180, 180])
+timer = 0
 
-while(True):
-    #screen = np.array(ImageGrab.grab(bbox=(97, 927, 167, 957)))
-    screen = cv2.imread("rain.png")
-    rgb = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
-    processImage(rgb)
-    if cv2.waitKey(25) & 0xFF == ord("q"):
-        cv2.destroyAllWindows()
-        break
+while timer < 15000000:
+    timer = timer + 1
+    if timer == 1:
+        screen = np.array(ImageGrab.grab(bbox=(97, 927, 167, 957)))
+        cv2.imshow('view', screen)
+        rgb = cv2.cvtColor(screen, cv2.COLOR_BGR2HSV)
+        processImage(rgb)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
+    if timer > 14999999:
+        timer = 0
